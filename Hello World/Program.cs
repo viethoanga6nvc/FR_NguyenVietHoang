ddf2b203d8827ab0d1b7cd7aa005c9a2057eba14
Hoàng N.V <viethoang.a6nvc@gmail.com>
2022-07-21 13:26:54 +0700

    Add project files.

---
+++ Hello World/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+string name = Console.ReadLine();
+Console.WriteLine("Hello " + name);
+Console.WriteLine("Welcom to NTQ Solution!");
+Hello();
+
+static void Hello()
+{
+
+}
